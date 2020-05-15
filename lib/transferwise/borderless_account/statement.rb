63154dd05cac3ca8d17a3334bf9bd13bb2f0f4b3
Thomas Belliard <thomas.belliard@sylogix.net>
2020-05-15 13:30:36 +0300

    Using transferwise API version 3 to get account statement. See https://api-docs.transferwise.com/#borderless-accounts-get-account-statement

--- lib/transferwise/borderless_account/statement.rb
+++ lib/transferwise/borderless_account/statement.rb
@@ -1,7 +1,13 @@
 module Transferwise
   class BorderlessAccount::Statement < APIResource
-    def self.collection_url(resource_id = nil)
-      "/#{API_VERSION}/borderless-accounts/statement"
+    def self.collection_url(profile_id:, borderless_account_id:)
+      "/v3/profiles/#{profile_id}/borderless-accounts/#{borderless_account_id}/statement"
     end
+
+    def self.list(filters = {}, headers = {}, profile_id:, borderless_account_id:)
+      response = Transferwise::Request.request(:get, collection_url(profile_id: profile_id, borderless_account_id: borderless_account_id), filters, headers)
+      convert_to_transferwise_object(response)
+    end
+
   end
 end
